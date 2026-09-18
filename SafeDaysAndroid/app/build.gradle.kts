a29796731c4eb2a9208b94f236c587d2c7aacfc1
BenjiGHG <benjighg45@gmail.com>
2026-09-18 15:36:04 +0200

    Add Safe Days Android project

---
+++ SafeDaysAndroid/app/build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.benjiGHG.safedays"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.benjiGHG.safedays"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.16.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation("com.google.android.material:material:1.13.0")
+}
